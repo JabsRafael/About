a6a7eef1c088e7a76f776d5dfbe359614d7c06fe
Jabison Rafael <jabsr@usp.br>
2024-01-02 18:50:18 -0300

    teste-staging

--- script.js
+++ script.js
@@ -4,15 +4,11 @@ function showCursor() {
     cursorElement.style.opacity = '1'; // Ou outra propriedade para mostrar o cursor
   }
 
-function enableBlur() {
-    canvas.style.filter = 'blur(1px)';
-}
 function showInfoContainer() {
     const infoContainer = document.getElementById('jsonResume');
     infoContainer.classList.remove('hidden');
     infoContainer.classList.add('visible');
   }
 
-setTimeout(enableBlur, 2000);
-setTimeout(showInfoContainer, 2000);
-setTimeout(showCursor, 2100);
+setTimeout(showInfoContainer, 500);
+setTimeout(showCursor, 600);
